f8a7a3301032b120734d70956cd07382f712a1e3
saadsgh123 <sdsghouri@gmail.com>
2024-10-28 23:20:09 +0100

    task 1 try 3 🥶

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,8 +2,6 @@
 """
 function should return a tuple of size
 """
-import csv
-from typing import List
 
 
 def index_range(page, page_size):
@@ -14,34 +12,3 @@ def index_range(page, page_size):
     :return: return in a list for those particular pagination parameters
     """
     return (page * page_size) - page_size, page * page_size
-
-
-class Server:
-    """Server class to paginate a database of popular baby names.
-    """
-    DATA_FILE = "Popular_Baby_Names.csv"
-
-    def __init__(self):
-        self.__dataset = None
-
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
-        return self.__dataset
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
-
-        index = index_range(page, page_size)
-        data = self.dataset()[index[0]:index[1]]
-        try:
-            return data
-        except IndexError:
-            return []
